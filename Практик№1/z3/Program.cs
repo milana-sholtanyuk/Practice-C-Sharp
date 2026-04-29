a0dd8033572520a2862ca4fb073a647047b11e16
milana-sholtanyuk <jungminseong7.1.0@gmail.com>
2026-04-29 14:43:30 +0300

    Add files via upload

---
+++ Практик№1/z3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите A (-5..5): ");
+        double A = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите N (1..10): ");
+        int N = int.Parse(Console.ReadLine());
+
+        double sum = 1;
+        double power = 1;
+
+        for (int i = 1; i <= N; i++)
+        {
+            power *= A;
+            sum += power;
+        }
+
+        Console.WriteLine($"Сумма = {sum:F4}");
+    }
+}
---
+++ Практик№1/z9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double A = 0;
+        double B = Math.PI / 4;
+        int M = 20;
+
+        double H = (B - A) / M;
+
+        Console.WriteLine("Табулирование функции F(x) = sin(x) - tg(x)");
+        Console.WriteLine($"Отрезок: [{A}, {B:F4}], шаг: {H:F4}\n");
+        Console.WriteLine("    x        F(x)");
+        Console.WriteLine("------------------");
+
+        double x = A;
+        for (int i = 0; i <= M; i++)
+        {
+            double y = Math.Sin(x) - Math.Tan(x);
+            Console.WriteLine($"{x:F4}    {y:F6}");
+            x += H;
+        }
+    }
+}
