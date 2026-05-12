5cebbcc4d8c91555490b0a70b77a5c587cc3ff68
milana-sholtanyuk <jungminseong7.1.0@gmail.com>
2026-05-12 10:09:20 +0300

    .

---
+++ DAY16/LibraryApp/Services/LibraryService.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApp.Models;
+
+namespace LibraryApp.Services
+{
+    public class LibraryService
+    {
+        private readonly DataStorageService _storage;
+        public ObservableCollection<BookModel> Books { get; private set; }
+
+        public LibraryService(DataStorageService storage)
+        {
+            _storage = storage;
+            var loaded = _storage.LoadBooks();
+            Books = new ObservableCollection<BookModel>(loaded);
+        }
+
+        private void SaveData()
+        {
+            _storage.SaveBooks(Books.ToList());
+        }
+
+        public async Task<ObservableCollection<BookModel>> SearchBooksAsync(string searchText, string filterAuthor, string filterGenre)
+        {
+            await Task.Delay(1500);
+
+            var query = Books.AsEnumerable();
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                query = query.Where(b =>
+                    (b.Title ?? "").ToLower().Contains(searchText.ToLower()) ||
+                    (b.Author ?? "").ToLower().Contains(searchText.ToLower()) ||
+                    (b.Genre ?? "").ToLower().Contains(searchText.ToLower()));
+            }
+
+            if (!string.IsNullOrWhiteSpace(filterAuthor))
+            {
+                query = query.Where(b => (b.Author ?? "").ToLower().Contains(filterAuthor.ToLower()));
+            }
+
+            if (!string.IsNullOrWhiteSpace(filterGenre))
+            {
+                query = query.Where(b => (b.Genre ?? "").ToLower().Contains(filterGenre.ToLower()));
+            }
+
+            return new ObservableCollection<BookModel>(query.ToList());
+        }
+
+        public void AddBook(BookModel book)
+        {
+            Books.Add(book);
+            SaveData();
+        }
+
+        public void UpdateBook(BookModel oldBook, BookModel newBook)
+        {
+            var index = Books.IndexOf(oldBook);
+            if (index >= 0)
+            {
+                Books[index] = newBook;
+                SaveData();
+            }
+        }
+
+        public void DeleteBook(BookModel book)
+        {
+            Books.Remove(book);
+            SaveData();
+        }
+
+        public void ToggleBookAvailability(BookModel book)
+        {
+            book.IsAvailable = !book.IsAvailable;
+            SaveData();
+        }
+    }
+}
---
+++ DAY16/LibraryApp/ViewModels/LibraryViewModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using LibraryApp.Models;
+using LibraryApp.Services;
+
+namespace LibraryApp.ViewModels
+{
+    public class LibraryViewModel : INotifyPropertyChanged
+    {
+        private readonly LibraryService _service;
+        private string _searchText;
+        private string _filterAuthor;
+        private string _filterGenre;
+        private ObservableCollection<BookModel> _books;
+        private bool _isLoading;
+        private BookModel _selectedBook;
+
+        public LibraryViewModel()
+        {
+            var storage = new DataStorageService();
+            _service = new LibraryService(storage);
+            _books = new ObservableCollection<BookModel>();
+
+            LoadBooksCommand = new RelayCommand(async () => await LoadBooksAsync());
+            AddBookCommand = new RelayCommand(OpenAddBookDialog);
+            EditBookCommand = new RelayCommand(OpenEditBookDialog, () => SelectedBook != null);
+            DeleteBookCommand = new RelayCommand(DeleteBook, () => SelectedBook != null);
+            ToggleAvailabilityCommand = new RelayCommand(ToggleAvailability, () => SelectedBook != null);
+
+            Task.Run(async () => await LoadBooksAsync());
+        }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set { _searchText = value; OnPropertyChanged(); Task.Run(async () => await LoadBooksAsync()); }
+        }
+
+        public string FilterAuthor
+        {
+            get => _filterAuthor;
+            set { _filterAuthor = value; OnPropertyChanged(); Task.Run(async () => await LoadBooksAsync()); }
+        }
+
+        public string FilterGenre
+        {
+            get => _filterGenre;
+            set { _filterGenre = value; OnPropertyChanged(); Task.Run(async () => await LoadBooksAsync()); }
+        }
+
+        public ObservableCollection<BookModel> Books
+        {
+            get => _books;
+            set { _books = value; OnPropertyChanged(); }
+        }
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set { _isLoading = value; OnPropertyChanged(); }
+        }
+
+        public BookModel SelectedBook
+        {
+            get => _selectedBook;
+            set
+            {
+                _selectedBook = value;
+                OnPropertyChanged();
+                ((RelayCommand)EditBookCommand).RaiseCanExecuteChanged();
+                ((RelayCommand)DeleteBookCommand).RaiseCanExecuteChanged();
+                ((RelayCommand)ToggleAvailabilityCommand).RaiseCanExecuteChanged();
+            }
+        }
+
+        public ICommand LoadBooksCommand { get; }
+        public ICommand AddBookCommand { get; }
+        public ICommand EditBookCommand { get; }
+        public ICommand DeleteBookCommand { get; }
+        public ICommand ToggleAvailabilityCommand { get; }
+
+        private async Task LoadBooksAsync()
+        {
+            IsLoading = true;
+            var result = await _service.SearchBooksAsync(SearchText, FilterAuthor, FilterGenre);
+            Books = result;
+            IsLoading = false;
+        }
+
+        private void OpenAddBookDialog()
+        {
+            var dialog = new BookDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                _service.AddBook(dialog.Book);
+                Task.Run(async () => await LoadBooksAsync());
+            }
+        }
+
+        private void OpenEditBookDialog()
+        {
+            var dialog = new BookDialog(SelectedBook);
+            if (dialog.ShowDialog() == true)
+            {
+                _service.UpdateBook(SelectedBook, dialog.Book);
+                Task.Run(async () => await LoadBooksAsync());
+            }
+        }
+
+        private void DeleteBook()
+        {
+            if (System.Windows.MessageBox.Show($"Удалить книгу \"{SelectedBook.Title}\"?", "Подтверждение",
+                System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Question) == System.Windows.MessageBoxResult.Yes)
+            {
+                _service.DeleteBook(SelectedBook);
+                Task.Run(async () => await LoadBooksAsync());
+            }
+        }
+
+        private void ToggleAvailability()
+        {
+            _service.ToggleBookAvailability(SelectedBook);
+            Task.Run(async () => await LoadBooksAsync());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
